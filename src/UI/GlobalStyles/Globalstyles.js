0b0629fb159f6e5b7e0e372dc65d74854a92bdfc
Sergey Shvets <justone128@gmail.com>
2019-11-28 03:01:51 +0200

    add UI components: GlobalStyles, Content

---
+++ src/UI/GlobalStyles/Globalstyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    color: #776e65;
+    background-color: #faf8ef;
+    font-family: "Clear Sans", "Helvetica Neue", Arial, sans-serif;
+    font-size: 18px;
+  }
+`
